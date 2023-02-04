04c06368be5f4fd71d5d11b6e01fd87bdf0805ae
jannati1 <akter15-5523@diu.edu.bd.com>
2023-02-05 00:52:39 +0600

    "Task1"

---
+++ Documents/NetBeansProjects/Task1/src/main/java/practise1/task1/Task1.java
@@ -0,0 +1,13 @@
+
+package practise1.task1;
+
+
+public class Task1 {
+     public static void main(String[] args) {
+        System.out.println("Name : Jannati nahid akter");
+        System.out.println("ID : 221-15-5523");
+        System.out.println("Adress : Ashulia,Dhaka");
+        System.out.println("Fovorite game : Football ");
+    }
+    
+}
