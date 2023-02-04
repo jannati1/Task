53f136161897fe1c6150dc9e90c8505c9f554bc0
jannati1 <akter15-5523@diu.edu.bd.com>
2023-02-05 01:02:24 +0600

    "Task2"

---
+++ Documents/NetBeansProjects/Task2/src/main/java/practise1/task2/Task2.java
@@ -0,0 +1,19 @@
+
+package practise1.task2;
+
+
+public class Task2 {
+    public static void main(String[] args) {
+        int num1, num2,sum,sub,mul,div;
+        num1 = 10;
+        num2 = 20;
+        sum = num1+num2;
+        sub = num1-num2;
+        mul = num1*num2;
+        div = num1/num2;
+        System.out.println("sum=" +sum);
+        System.out.println("sub=" +sub);
+        System.out.println("mul=" +mul);
+        System.out.println("div=" +div);
+    }
+}
