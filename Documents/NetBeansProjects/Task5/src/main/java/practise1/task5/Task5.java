af1af63aac15effed577ef6132274540f34c6c8a
jannati1 <akter15-5523@diu.edu.bd.com>
2023-02-05 01:23:30 +0600

    "Task5"

---
+++ Documents/NetBeansProjects/Task5/src/main/java/practise1/task5/Task5.java
@@ -0,0 +1,27 @@
+
+package practise1.task5;
+
+
+public class Task5 {
+    public static void main(String[] args) {
+        int A = 5;
+        int B = 6;
+        int C = 7;
+        int D = 8;
+        System.out.println("A = 5");
+        System.out.println("B = 6");
+        System.out.println("C = 7");
+        System.out.println("D = 8");
+       int result1 = (A*B - C*D);
+       int result2 = 2*A - B + 3*D;
+       int result3 = A*A + B*B - C*C + D*D;
+       int result4 = A*A*A + B - C*C;
+        System.out.println("(A*B - C*D) ="+result1);
+        System.out.println("2A - B +3D ="+result2);
+        System.out.println("A*A + B*B - C*C + D*D ="+result3);
+        System.out.println("A*A*A + B - C*C ="+result4);
+        
+        
+    }
+    
+}
