8b1a334218ebaaa89287121337a34fc25507a507
jannati1 <akter15-5523@diu.edu.bd.com>
2023-02-05 01:06:43 +0600

    "Task3"

---
+++ Documents/NetBeansProjects/Task3/src/main/java/practise1/task3/Task3.java
@@ -0,0 +1,17 @@
+
+package practise1.task3;
+
+import java.util.Scanner;
+
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your Name :");
+        String name = input .nextLine();
+        System.out.println("\nYour Information");
+        System.out.println("Name : "+name);
+        
+    }
+    
+}
